e8797c5d1d36598c966ec4a4cdc6282a96a1dae4
Gui Costa <guibscosta@hotmail.com>
2024-11-20 14:51:29 +0000

    Update PlantData.cs

--- ProfitCalculator/main/models/PlantData.cs
+++ ProfitCalculator/main/models/PlantData.cs
@@ -326,7 +326,6 @@ namespace ProfitCalculator.main.models
             UtilsSeason season = Container.Instance.GetInstance<Calculator>(ModEntry.UniqueID)?.Season ?? UtilsSeason.Spring;
             uint day = Container.Instance.GetInstance<Calculator>(ModEntry.UniqueID)?.Day ?? 0;
             if (season == UtilsSeason.Greenhouse || Seasons.Count == 1) { return 1; }
-                return 1;
             else
             {
                 return (int)Math.Ceiling(TotalAvailableDays(season, (int)day) / 28.0);
