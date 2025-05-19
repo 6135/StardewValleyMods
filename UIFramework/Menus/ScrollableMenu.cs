b8b6449629b526fc20ab6a7c3ebd8ab4004a000b
Gui Costa <guibscosta@hotmail.com>
2025-05-19 19:35:21 +0100

    Comment out ScrollableMenu.cs implementation
    The entire implementation of the `ScrollableMenu` class has been commented out, including all using directives, the namespace, properties, methods, and event declarations. This change effectively disables the class while preserving the code for potential future reference or restoration.

--- UIFramework/Menus/ScrollableMenu.cs
+++ UIFramework/Menus/ScrollableMenu.cs
@@ -1,42 +1,42 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UIFramework.Components.Base;
+﻿//using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Graphics;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using UIFramework.Components.Base;
 
-namespace UIFramework.Menus
-{
-    public class ScrollableMenu : BaseMenu
-    {
-        public int ContentHeight { get; protected set; }
-        public int ViewportHeight { get; protected set; }
-        public int ScrollPosition { get; set; }
-        public int ScrollStep { get; set; } = 20;
-        public bool ShowScrollbar { get; set; } = true;
-        public Color ScrollbarColor { get; set; } = Color.Gray;
-        public Color ScrollbarBackgroundColor { get; set; } = new Color(0, 0, 0, 100);
-        public int ScrollbarWidth { get; set; } = 20;
+//namespace UIFramework.Menus
+//{
+//    public class ScrollableMenu : BaseMenu
+//    {
+//        public int ContentHeight { get; protected set; }
+//        public int ViewportHeight { get; protected set; }
+//        public int ScrollPosition { get; set; }
+//        public int ScrollStep { get; set; } = 20;
+//        public bool ShowScrollbar { get; set; } = true;
+//        public Color ScrollbarColor { get; set; } = Color.Gray;
+//        public Color ScrollbarBackgroundColor { get; set; } = new Color(0, 0, 0, 100);
+//        public int ScrollbarWidth { get; set; } = 20;
 
-        public event Action<int> ScrollChanged;
+//        public event Action<int> ScrollChanged;
 
-        public override void draw(SpriteBatch b);
+//        public override void draw(SpriteBatch b);
 
-        public override void receiveScrollWheelAction(int direction);
+//        public override void receiveScrollWheelAction(int direction);
 
-        public void ScrollToTop();
+//        public void ScrollToTop();
 
-        public void ScrollToBottom();
+//        public void ScrollToBottom();
 
-        public void ScrollTo(int position);
+//        public void ScrollTo(int position);
 
-        public void RecalculateContentHeight();
+//        public void RecalculateContentHeight();
 
-        // Handle components that are outside the visible area
-        protected bool IsComponentVisible(BaseComponent component);
+//        // Handle components that are outside the visible area
+//        protected bool IsComponentVisible(BaseComponent component);
 
-        protected override void DrawComponents(SpriteBatch b);
-    }
-}
+//        protected override void DrawComponents(SpriteBatch b);
+//    }
+//}
