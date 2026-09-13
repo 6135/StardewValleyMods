2185ad6c0aadf2a0638d22fb58cb93e7666a35ce
Gui Costa <12702456+6135@users.noreply.github.com>
2026-09-13 22:46:33 +0100

    Moved Arch into project folder

---
+++ StardewUIFramework/Config.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+
+namespace StardewUIFramework
+{
+    internal class Config
+    {
+        public SButton debugKey { get; set; }
+
+        public Config()
+        {
+            debugKey = SButton.J;
+        }
+    }
+}
